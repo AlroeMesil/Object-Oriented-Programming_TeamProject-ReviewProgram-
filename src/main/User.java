e57805b8d944df2b35f29f4869d4fa322bc5fcd9
jiyoxn <jiyoxn03@naver.com>
2023-11-19 21:49:58 +0900

    User, UserData 클래스 리팩토링

--- src/main/User.java
+++ src/main/User.java
@@ -1,77 +1,36 @@
 package main;
 
-public class User {
-	String id;
-    private String pw;
-    private String name;
-    private String nickName;
-    private String email;
-    
-    public User() {
-		// TODO Auto-generated constructor stub
-		
-	}
+import mgr.Manageable;
 
-    public User(String id, String pw, String name, String nickName, String email) {
-        setId(id);
-        setPw(pw);
-        setName(name);
-        setNickName(nickName);
-        setEmail(email);
+import java.util.Scanner;
+
+public class User implements Manageable {
+    String id;
+    String pw;
+    String name;
+    String nickName;
+    String email;
+    Integer userLike=0;
+
+    User(String id, String pw, String name, String nickName, String email) {
+        this.id=id;
+        this.pw=pw;
+        this.name=name;
+        this.nickName=nickName;
+        this.email=email;
     }
-
     public User(String id) {
-        setId(id);
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
         this.id = id;
-    }
-
-    public String getPw() {
-        return pw;
-    }
-
-    public void setPw(String pw) {
-        this.pw = pw;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getNickName() {
-        return nickName;
-    }
-
-    public void setNickName(String nickName) {
-        this.nickName = nickName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 
     @Override
     public boolean equals(Object o) {
-        if (o == null || !(o instanceof User)) {
+        if (o == null || !(o instanceof main.User)) {
             return false;
         }
-        User temp = (User) o;
+        main.User temp = (main.User) o;
 
-        return id.equals(temp.getId());
+        return id.equals(temp.id);
     }
 
     @Override
@@ -81,6 +40,35 @@ public class User {
         info += "Name: " + name + "\n";
         info += "NickName: " + nickName + "\n";
         info += "email: " + email + "\n";
+        info += "userLike: " + userLike + "\n";
         return info;
+    }
+
+    @Override
+    public void read(Scanner scan) {
+        this.id=scan.next();
+        this.pw=scan.next();
+        this.name=scan.next();
+        this.nickName=scan.next();
+        this.email=scan.next();
+//        this.userLike=scan.nextInt();
+    }
+    //User출력
+    @Override
+    public void print() {
+        System.out.println(
+                "ID: " + id + ", Name: " + name + ", 닉네임:" + nickName+
+                        ", Email: " + email+", UserLike: "+userLike);
+    }
+    //ID, PW랑 kwd비교
+    @Override
+    public boolean matches(String kwd) {
+        if (this.id.equals(kwd)) {
+            return true;
+        }
+        if (this.pw.equals(kwd)) {
+            return true;
+        }
+        return false;
     }
 }
